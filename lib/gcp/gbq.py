8ae666c93b38f1bfc6db0c5bd69a7bfd2307b0e0
philip.carrington <philip.carrington@holidayextras.com>
2020-03-23 08:02:03 +0000

    getting the permissions for the dataset

--- lib/gcp/gbq.py
+++ lib/gcp/gbq.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 # from airflow import DAG  # noqa: F401
 
+import json
 from google.cloud import bigquery
 
 
@@ -9,21 +10,15 @@ def create_google_client_connection(project):
     client = bigquery.Client(
         project=project
     )
-
     return client
 
 
 def get_project_datasets(gcp_name):
     gbq_connection = create_google_client_connection(gcp_name)
-
     datasets = list(gbq_connection.list_datasets())  # Make an API request.
     project = gbq_connection.project
 
     if datasets:
-        #print(type(datasets))
-        #print("Datasets in project {}:".format(project))
-        #for dataset in datasets:
-        #    print("\t{}".format(dataset.dataset_id))
         return datasets
     else:
         print("{} project does not contain any datasets.".format(project))
@@ -35,19 +30,25 @@ def get_dataset_name(dataset_data):
 
 def get_datasets_information(gcp_name, gbq_dataset_name):
     gbq_connection = create_google_client_connection(gcp_name)
-
-    dataset_name = get_dataset_name(gbq_dataset_name)
-
+    # dataset_name = get_dataset_name(gbq_dataset_name)
     dataset_object = gbq_connection.get_dataset(dataset_name)
-
     return dataset_object
+
 
 def get_dataset_friendly_name(dataset_data):
     return dataset_data.friendly_name
 
 
 def get_dataset_current_permissions(dataset_data):
-    return list(dataset_data.access_entries)
+    return dataset_data.__dict__
+
+
+def permissions_list_to_json(permissions_list):
+    for p in permissions_list:
+        print(type(p))
+        print(p)
+
+    # return json.dumps(permissions_list)
 
 
 def get_dataset_current_description(dataset_data):
@@ -64,9 +65,39 @@ for project_dataset in project_datasets:
     print("************************ START *********************")
     print("************************ DATASET INFO *********************")
     print('The dataset is: {}'.format(get_dataset_name(project_dataset.dataset_id)))
-    dataset_info = get_datasets_information('hx-trial-dev-pmc', project_dataset.dataset_id)
-    print(get_dataset_friendly_name(dataset_info))
-    print(get_dataset_current_permissions(dataset_info))
-    print(get_dataset_current_description(dataset_info))
-    print(get_dataset_current_labels(dataset_info))
+
+    dataset_name = get_dataset_name(project_dataset.dataset_id)
+    dataset_info = get_datasets_information('hx-trial-dev-pmc', dataset_name)
+    dataset_friendly_name = get_dataset_friendly_name(dataset_info)
+    dataset_current_description = get_dataset_current_description(dataset_info)
+    dataset_current_labels = get_dataset_current_labels(dataset_info)
+    dataset_current_permissions = get_dataset_current_permissions(dataset_info)
+    dataset_current_permissions_json = permissions_list_to_json(dataset_current_permissions)
+
+
+    print(type(dataset_name))
+    print(dataset_name)
+    print(type(dataset_friendly_name))
+    print(dataset_friendly_name)
+    print(type(dataset_current_description))
+    print(dataset_current_description)
+    print(type(dataset_current_labels))
+    print(dataset_current_labels)
+    print(type(dataset_current_permissions))
+    print(dataset_current_permissions)
+
+    for p in dataset_current_permissions:
+        print(type(p))
+        print(p)
+        # Class base need to do a test
+        permission_dict = p.__dict__
+        print(permission_dict)
+        print(p.__dict__["role"])
+        print(permission_dict["role"])
+        print(permission_dict["entity_type"])
+        print(permission_dict["entity_id"])
+
+    print(type(dataset_current_permissions_json))
+    print(dataset_current_permissions_json)
+
     print("************************ END *********************")
