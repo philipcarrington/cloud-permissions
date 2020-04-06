a1588a6bcfeb888bc5ea0a248108df34621ad115
philip.carrington <philip.carrington@holidayextras.com>
2020-04-06 07:27:48 +0100

    started weiting to the database

--- gbq-permissions/add_current_state_to_database.py
+++ gbq-permissions/add_current_state_to_database.py
@@ -16,7 +16,8 @@ project_name = 'hx-data-production'
 # See if the project exists in the database or not
 project_id = get_existing_google_cloud_project_id(configs_dir, project_name)
 if project_id:
-    print('EXISTS')
+    print('Project EXISTS')
+    print(project_id)
 else:
     print('Needs adding to Db')
     project_id = add_existing_google_cloud_project_id(configs_dir, project_name)
@@ -26,49 +27,34 @@ project_datasets = get_project_datasets(project_name)
 
 # Cycle round the datasets and add detail to the database:
 for project_dataset in project_datasets:
-
-
-
+    # Get the dataset proper name:
     dataset_name = get_dataset_name(project_dataset.dataset_id)
-    # print(type(dataset_name))
-    # print(type(dataset_name.decode('utf-8')))
+    print(dataset_name)
+
+    # Get the the information on the dataset:
     dataset_info = get_datasets_information(project_name, dataset_name)
+    print(dataset_info)
+    # Extract the data from the information:
     dataset_resource_type = get_dataset_resource_type_str(dataset_info)
     dataset_project = get_dataset_project_str(dataset_info)
     dataset_id = get_dataset_id_str(dataset_info)
     dataset_creation_time = get_dataset_creation_time_int(dataset_info)
-    dataset_access_list= get_dataset_access_list_list(dataset_info)
+    dataset_access_list = get_dataset_access_list_list(dataset_info)
     dataset_etag_str = get_dataset_etag_str(dataset_info)
-    dataset_location_str= get_dataset_location_str(dataset_info)
-    dataset_last_modified_time_str= get_dataset_last_modified_time_str(dataset_info)
+    dataset_location_str = get_dataset_location_str(dataset_info)
+    dataset_last_modified_time_str = get_dataset_last_modified_time_str(dataset_info)
     dataset_full_id_str = get_dataset_full_id_str(dataset_info)
     dataset_self_link_str = get_dataset_self_link_str(dataset_info)
 
+    # Get the database dataset id:
+    db_dataset_id = get_existing_google_cloud_dataset_id(configs_dir, dataset_name, project_id)
+    if db_dataset_id:
+        print('DATASET EXISTS')
+        print(db_dataset_id)
+    else:
+        db_dataset_id = add_existing_google_cloud_dataset_id(configs_dir, dataset_name, project_id)
 
-    print(type(dataset_name))
-    print(dataset_name)
-    print(type(dataset_info))
-    print(dataset_info)
-    print(type(dataset_resource_type))
-    print(dataset_resource_type)
-    print(type(dataset_project))
-    print(dataset_project)
-    print(type(dataset_id))
-    print(dataset_id)
-    print(type(dataset_creation_time))
-    print(dataset_creation_time)
-    print(type(dataset_access_list))
-    print(dataset_access_list)
-    print(type(dataset_etag_str))
-    print(dataset_etag_str)
-    print(type(dataset_location_str))
-    print(dataset_location_str)
-    print(type(dataset_last_modified_time_str))
-    print(dataset_last_modified_time_str)
-    print(type(dataset_full_id_str))
-    print(dataset_full_id_str)
-    print(type(dataset_self_link_str))
-    print(dataset_self_link_str)
+    print(db_dataset_id)
 
     print("Iterate through the access list:")
     special_groups, group_groups, user_groups, views = process_dataset_access_list(dataset_access_list)
--- gbq-permissions/mysql_db_manager/python_mysql_existing.py
+++ gbq-permissions/mysql_db_manager/python_mysql_existing.py
@@ -24,12 +24,13 @@ def get_existing_google_cloud_project_id(configs_dir, project_name):
         print(e)
 
 
-def get_existing_google_cloud_dataset_id(configs_dir, project_name):
+def get_existing_google_cloud_dataset_id(configs_dir, dataset_name, project_id):
     try:
         # Create the query string:
-        query_string = "select existing_google_cloud_project_id " \
-                       "from existing_google_cloud_projects " \
-                       "where existing_google_cloud_project_name = '{}'".format(project_name)
+        query_string = "select existing_google_big_query_dataset_id " \
+                       "from existing_google_big_query_datasets " \
+                       "where existing_google_big_query_dataset_name = '{}'" \
+                       "    and existing_google_big_query_project_id = {}".format(dataset_name, project_id)
 
         db_config = read_db_config(
             filename='{}/mysql_config.ini'.format(configs_dir)
@@ -51,6 +52,33 @@ def add_existing_google_cloud_project_id(configs_dir, project_name):
         # Create the query string:
         query_string = "insert into existing_google_cloud_projects (existing_google_cloud_project_name) " \
                        "values ('{}')".format(project_name)
+
+        db_config = read_db_config(
+            filename='{}/mysql_config.ini'.format(configs_dir)
+        )
+        db_connection = MySQLConnection(**db_config)
+        cursor = db_connection.cursor()
+        cursor.execute(query_string)
+
+        if cursor.lastrowid:
+            return cursor.lastrowid
+        else:
+            print('last insert id not found')
+
+        db_connection.commit()
+    except Error as e:
+        print(e)
+    finally:
+        cursor.close()
+        db_connection.close()
+
+
+def add_existing_google_cloud_dataset_id(configs_dir, dataset_name, project_id):
+    try:
+        # Create the query string:
+        query_string = "insert into existing_google_big_query_datasets " \
+                       "(existing_google_big_query_project_id, existing_google_big_query_dataset_name) " \
+                       "values ({}, '{}')".format(project_id, dataset_name)
 
         db_config = read_db_config(
             filename='{}/mysql_config.ini'.format(configs_dir)
