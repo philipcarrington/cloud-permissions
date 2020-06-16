ddf752f8d36146756b8f9b8c6ee4e1c66d4c69df
philip.carrington <philip.carrington@holidayextras.com>
2020-06-16 12:01:48 +0100

    got the creator script started

--- database/create/v2.0.0/9-gbq-dataset-create-update.sql
+++ database/create/v2.0.0/9-gbq-dataset-create-update.sql
@@ -1,6 +1,6 @@
-drop view permissions_meta.gbq_dataset_create_update_commands
+drop view permissions_meta.gbq_dataset_create_update_commands_command_line
 go
-create view permissions_meta.gbq_dataset_create_update_commands as (
+create view permissions_meta.gbq_dataset_create_update_commands_command_line as (
 with 
     constants as (
         select '/tmp/' as permissions_file_output_dir
@@ -110,7 +110,7 @@ with
                odted.o_organisation_identifier as organisation_identifier,
                edd.e_family_name as dataset_name_pt1,
                edd.e_given_name as dataset_name_pt2,
-               null as permissions_email_override,              
+               lower(concat(edd.e_given_name, '.', edd.e_family_name, '@', odted.o_organisation_email_domain)) as permissions_email_override,              
                odted.o_organisation_email_domain as organisation_email_domain
         from permissions_meta.employee_datasets_denorm edd    
             inner join permissions_meta.organisation_department_team_employees_denorm odted
@@ -244,6 +244,7 @@ select dd.internal_google_cloud_project_id,
        dd.google_cloud_project_name,
        dd.internal_google_bigquery_dataset_id,
        dd.google_bigquery_dataset_name,
+       
        concat(
                 'bq ', 
                 dd.location_command_part,
@@ -255,7 +256,7 @@ select dd.internal_google_cloud_project_id,
              ) as create_dataset_command,
        concat(
                 'echo ''{ "access": [', 
-                dpg.project_dataset_all_permissions ,
+                dpg.project_dataset_all_permissions,
                 '] }'' > ', 
                 dpg.permissions_filepath
               ) as create_dataset_permissions_file_command,
@@ -277,5 +278,182 @@ from dataset_data dd
         on dd.internal_google_cloud_project_id = dpg.internal_google_cloud_project_id
             and dd.internal_google_bigquery_dataset_id = dpg.internal_google_bigquery_dataset_id
 )            
-                           
+go
+
+drop view permissions_meta.gbq_dataset_create_update_commands_python
+go
+create view permissions_meta.gbq_dataset_create_update_commands_python as (
+    with     
+        dataset_data_base as (
+            /*
+                Organistaion Department Team:
+            */        
+            select 
+                   -- Identifiers:
+                   odtdd.gbd_google_cloud_project_id as internal_google_cloud_project_id,
+                   odtdd.gcp_google_cloud_project_name as google_cloud_project_name,
+                   odtdd.gbd_google_bigquery_dataset_id as internal_google_bigquery_dataset_id,
+                   odtdd.gbd_google_bigquery_dataset_name as google_bigquery_dataset_name,
+                   -- Dataset Detail:
+                   odtdd.gcl_google_cloud_location_name as google_cloud_location_name,
+                   odtdd.gbd_google_bigquery_dataset_default_table_expiration as google_bigquery_dataset_default_table_expiration,
+                   odtdd.gbd_google_bigquery_dataset_default_partition_expiration as google_bigquery_dataset_default_partition_expiration,
+                   odtdd.gbd_google_bigquery_dataset_short_description as google_bigquery_dataset_short_description,
+                   -- Permissions Details:
+                   odtdd.gcr_google_cloud_role_role as role_type,
+                   '"groupByEmail"' as permission_type,
+                   'team' as dataset_prefix,
+                   odtdd.o_organisation_identifier as organisation_identifier,
+                   odtdd.d_department_name as dataset_name_pt1,
+                   odtdd.t_team_name as dataset_name_pt2,
+                   null as permissions_email_override,               
+                   odtdd.o_organisation_email_domain as organisation_email_domain
+            from permissions_meta.organisation_department_team_datasets_denorm odtdd
+            union
+            /*
+                Groups:
+            */
+            select distinct  
+                   -- Identifiers:
+                   gdd.gbd_google_cloud_project_id as internal_google_cloud_project_id,
+                   gdd.gcp_google_cloud_project_name as google_cloud_project_name,
+                   gdd.gbd_google_bigquery_dataset_id as internal_google_bigquery_dataset_id,
+                   gdd.gbd_google_bigquery_dataset_name as google_bigquery_dataset_name,
+                   -- Dataset Detail:
+                   gdd.gcl_google_cloud_location_name as google_cloud_location_name,
+                   gdd.gbd_google_bigquery_dataset_default_table_expiration as google_bigquery_dataset_default_table_expiration,
+                   gdd.gbd_google_bigquery_dataset_default_partition_expiration as google_bigquery_dataset_default_partition_expiration,
+                   gdd.gbd_google_bigquery_dataset_short_description as google_bigquery_dataset_short_description,
+                   -- Permissions Details:
+                   gdd.gcr_google_cloud_role_role as role_type,
+                   '"groupByEmail"' as permission_type,
+                   gdd.g_group_prefix as dataset_prefix,
+                   odted.o_organisation_identifier as organisation_identifier,
+                   gdd.g_group_name as dataset_name_pt1,
+                   null as dataset_name_pt2,
+                   null as permissions_email_override,              
+                   odted.o_organisation_email_domain as organisation_email_domain
+            from permissions_meta.group_datasets_denorm gdd
+                 inner join permissions_meta.employee_groups eg 
+                    on gdd.g_group_id = eg.group_id
+                inner join permissions_meta.organisation_department_team_employees_denorm odted
+                    on eg.employee_id = odted.e_employee_id
+            union
+            /*
+                Jobs:
+            */
+            select distinct
+                   -- Identifiers:
+                   jdd.gbd_google_cloud_project_id as internal_google_cloud_project_id,
+                   jdd.gcp_google_cloud_project_name as google_cloud_project_name,
+                   jdd.gbd_google_bigquery_dataset_id as internal_google_bigquery_dataset_id,
+                   jdd.gbd_google_bigquery_dataset_name as google_bigquery_dataset_name,
+                   -- Dataset Detail:
+                   jdd.gcl_google_cloud_location_name as google_cloud_location_name,
+                   jdd.gbd_google_bigquery_dataset_default_table_expiration as google_bigquery_dataset_default_table_expiration,
+                   jdd.gbd_google_bigquery_dataset_default_partition_expiration as google_bigquery_dataset_default_partition_expiration,
+                   jdd.gbd_google_bigquery_dataset_short_description as google_bigquery_dataset_short_description,
+                   -- Permissions Details:
+                   jdd.gcr_google_cloud_role_role as role_type,
+                   '"groupByEmail"' as permission_type,
+                   'job' as dataset_prefix,
+                   odted.o_organisation_identifier as organisation_identifier,
+                   jdd.j_job_name as dataset_name_pt1,
+                   null as dataset_name_pt2,
+                   null as permissions_email_override,              
+                   odted.o_organisation_email_domain as organisation_email_domain
+            from permissions_meta.job_datasets_denorm jdd
+                inner join permissions_meta.employee_jobs ej
+                    on jdd.j_job_id = ej.job_id    
+                inner join permissions_meta.organisation_department_team_employees_denorm odted
+                    on ej.employee_id = odted.e_employee_id 
+            union
+            /*
+                Employees:
+            */
+            select distinct
+                   -- Identifiers:
+                   edd.gbd_google_cloud_project_id as internal_google_cloud_project_id,
+                   edd.gcp_google_cloud_project_name as google_cloud_project_name,
+                   edd.gbd_google_bigquery_dataset_id as internal_google_bigquery_dataset_id,
+                   edd.gbd_google_bigquery_dataset_name as google_bigquery_dataset_name,
+                   -- Dataset Detail:
+                   edd.gcl_google_cloud_location_name as google_cloud_location_name,
+                   edd.gbd_google_bigquery_dataset_default_table_expiration as google_bigquery_dataset_default_table_expiration,
+                   edd.gbd_google_bigquery_dataset_default_partition_expiration as google_bigquery_dataset_default_partition_expiration,
+                   edd.gbd_google_bigquery_dataset_short_description as google_bigquery_dataset_short_description,
+                   -- Permissions Details:
+                   edd.gcr_google_cloud_role_role as role_type,
+                   '"userByEmail"' as permission_type,
+                   'emp' as dataset_prefix,
+                   odted.o_organisation_identifier as organisation_identifier,
+                   edd.e_family_name as dataset_name_pt1,
+                   edd.e_given_name as dataset_name_pt2,
+                   lower(concat(edd.e_given_name, '.', edd.e_family_name, '@', odted.o_organisation_email_domain)) as permissions_email_override,              
+                   odted.o_organisation_email_domain as organisation_email_domain
+            from permissions_meta.employee_datasets_denorm edd    
+                inner join permissions_meta.organisation_department_team_employees_denorm odted
+                    on edd.e_employee_id = odted.e_employee_id 
+            union
+            /*
+                Service Accounts:
+            */
+            select distinct
+                   -- Identifiers:
+                   sadd.gbd_google_cloud_project_id as internal_google_cloud_project_id,
+                   sadd.gcp_google_cloud_project_name as google_cloud_project_name,
+                   sadd.gbd_google_bigquery_dataset_id as internal_google_bigquery_dataset_id,
+                   sadd.gbd_google_bigquery_dataset_name as google_bigquery_dataset_name,
+                   -- Dataset Detail:
+                   sadd.gcl_google_cloud_location_name as google_cloud_location_name,
+                   sadd.gbd_google_bigquery_dataset_default_table_expiration as google_bigquery_dataset_default_table_expiration,
+                   sadd.gbd_google_bigquery_dataset_default_partition_expiration as google_bigquery_dataset_default_partition_expiration,
+                   sadd.gbd_google_bigquery_dataset_short_description as google_bigquery_dataset_short_description,
+                   -- Permissions Details:
+                   sadd.gcr_google_cloud_role_role as role_type,
+                   '"userByEmail"' as permission_type,
+                   'sa' as dataset_prefix,
+                   odtgsa.o_organisation_identifier as organisation_identifier,
+                   sadd.gcsa_google_cloud_service_account_name as dataset_name_pt1,
+                   null as dataset_name_pt2,
+                   sadd.gcsa_google_cloud_service_account_email as permissions_email_override,
+                   odtgsa.o_organisation_email_domain as organisation_email_domain
+            from permissions_meta.service_account_datasets_denorm sadd    
+                inner join permissions_meta.organisation_department_team_google_service_accounts_denorm odtgsa
+                    on sadd.gcsa_google_cloud_service_account_id = odtgsa.gcsa_google_cloud_service_account_id                                                                                                    
+        ),
+        dataset_data as (
+            select ddb.internal_google_cloud_project_id,
+                   ddb.google_cloud_project_name,
+                   ddb.internal_google_bigquery_dataset_id,
+                   ddb.google_bigquery_dataset_name,
+                   ddb.google_cloud_location_name,
+                   ddb.google_bigquery_dataset_default_table_expiration,
+                   ddb.google_bigquery_dataset_default_partition_expiration,
+                   ddb.google_bigquery_dataset_short_description,
+                   ddb.role_type,
+                   ddb.permission_type,
+                   concat(
+                            ddb.dataset_prefix, '-',
+                            ddb.organisation_identifier, '-'                                                                  
+                   ) as authorised_group_name_start,
+                   replace(
+                           lower(
+                                   case when (ddb.dataset_name_pt2 is not null) then 
+                                        concat(ddb.dataset_name_pt1, '-', ddb.dataset_name_pt2)
+                                   else
+                                       ddb.dataset_name_pt1
+                                   end 
+                                ),
+                           ' ',
+                           '-'
+                           )                             
+                            as authorised_group_name_end,
+                   ddb.organisation_email_domain,
+                   ddb.permissions_email_override
+            from dataset_data_base ddb
+        )
+    select *
+    from dataset_data     
+)                                    
             
--- scripts/set_permissions.py
+++ scripts/set_permissions.py
@@ -1,12 +1,24 @@
+# For larking around with files:
 import os
 from configparser import ConfigParser
+
+# For MySQL querying:
 from mysql.connector import MySQLConnection, Error
 
-def get_configs_dir_location():
+# For connecting to Google using the service account:
+from google.oauth2 import service_account
+
+# For BigQuery Python API:
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
+
+###############################################################################
+# General functions:
+def get_dir_location(foldername):
     # Current Dir:
     current_dir = os.path.dirname(__file__)
     # Config files location:
-    return os.path.join(current_dir, 'configs')
+    return os.path.join(current_dir, foldername)
 
 def read_db_config(filename='db_cfg/config.ini', section='mysql'):
     """ Read database configuration file and return a dictionary object
@@ -29,13 +41,147 @@ def read_db_config(filename='db_cfg/config.ini', section='mysql'):
 
     return db
 
+def api_call_logger(google_api_action, internal_google_cloud_project_id, internal_google_bigquery_dataset_id, api_call_logger_message):
+    try:
+        # Get the config file directory for the MySQL connection:
+        configs_dir = get_dir_location('configs')
+        # Create the MySQL config filepath:
+        mysql_config_filepath = os.path.join(configs_dir, 'mysql_config.ini')
+
+        # Create the query
+        log_query = "insert into permissions_history.api_dataset_actions(" \
+                    "   api_dataset_action_datetime," \
+                    "   api_dataset_action_action_id," \
+                    "   internal_project_id," \
+                    "   internal_dataset_id," \
+                    "   api_action_state_description" \
+                    ")" \
+                    "values(" \
+                    "   now()," \
+                    "   (select a.action_id from permissions_history.actions a where action_name = '{}')," \
+                    "   {}," \
+                    "   {}," \
+                    "   '{}'" \
+                    ")".format(google_api_action, internal_google_cloud_project_id,
+                               internal_google_bigquery_dataset_id, api_call_logger_message)
+
+        # Set the connection properties:
+        db_config = read_db_config(
+            filename=mysql_config_filepath
+        )
+
+        # Create the connection
+        db_connection = MySQLConnection(**db_config)
+
+        db_connection.autocommit = True
+        cursor = db_connection.cursor()
+        cursor.execute(log_query)
+
+        if cursor.lastrowid:
+            return cursor.lastrowid
+        else:
+            print('last insert id not found')
+
+        db_connection.commit()
+        db_connection.close()
+
+    except Error as e:
+        print(e)
+    finally:
+        if db_connection.is_connected():
+            db_connection.close()
+            cursor.close()
+
+###############################################################################
+# Google Auth functions:
+def get_google_sa_account_creds(sa_json_key_filename):
+    # Get the secrets dir:
+    secrets_dir = get_dir_location('secrets')
+
+    # Get the filepath
+    json_key_filepath = os.path.join(secrets_dir, sa_json_key_filename)
+
+    credentials = service_account.Credentials.from_service_account_file(
+        json_key_filepath,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+
+    return credentials
+
+def get_client_connection(sa_credentials_filepath):
+    # Get the credentials:
+    credentials = get_google_sa_account_creds(sa_credentials_filepath)
+    client = bigquery.Client(
+        credentials=credentials,
+        project=credentials.project_id,
+    )
+
+    return client
+
+###############################################################################
+# BigQuery functions:
+def dataset_exists(dataset_id):
+    # Get client connection:
+    client = get_client_connection('resources-and-permissions-service-key.json')
+
+    try:
+        client.get_dataset(dataset_id)  # Make an API request.
+        # print("Dataset {} already exists".format(dataset_id))
+        return 1
+    except NotFound:
+        # print("Dataset {} is not found".format(dataset_id))
+        return 0
+
+def create_dataset(
+        dataset_id,
+        dataset_location,
+        dataset_default_table_expiration,
+        default_partition_expiration,
+        dataset_short_description="None at this time"
+):
+    # Get client connection:
+    client = get_client_connection('resources-and-permissions-service-key.json')
+
+    # Construct a full Dataset object to send to the API.
+    dataset = bigquery.Dataset(dataset_id)
+
+    # Set the dataset options:
+    dataset.location = dataset_location
+    # TODO:
+    # dataset.default_partition_expiration_ms = dataset_default_table_expiration
+    # dataset.default_table_expiration_ms = default_partition_expiration
+    dataset.description = dataset_short_description
+
+    # Try and create the dataset:
+    try:
+        client.create_dataset(dataset)  # Make an API request.
+        return 1
+    except Error as e:
+        print(e)
+        return 0
+
+###############################################################################
+# Permission setting functions:
 def get_permissions(project_name):
     # Get the config file directory for the MySQL connection:
-    configs_dir = get_configs_dir_location()
+    configs_dir = get_dir_location('configs')
     # Create the MySQL config filepath:
     mysql_config_filepath = os.path.join(configs_dir, 'mysql_config.ini')
 
     # Create the MySQL query string:
+    query_string = "select distinct " \
+                   "internal_google_cloud_project_id," \
+                   "    google_cloud_project_name," \
+                   "    internal_google_bigquery_dataset_id," \
+                   "    google_bigquery_dataset_name," \
+                   "    google_cloud_location_name," \
+                   "    google_bigquery_dataset_default_table_expiration," \
+                   "    google_bigquery_dataset_default_partition_expiration," \
+                   "    google_bigquery_dataset_short_description " \
+                   "from permissions_meta.gbq_dataset_create_update_commands_python gdcuc " \
+                   "where gdcuc.google_cloud_project_name = '{}'".format(project_name)
+
+    # print('Executing Query: {}'.format(query_string))
 
     # Set the connection properties:
     db_config = read_db_config(
@@ -45,10 +191,42 @@ def get_permissions(project_name):
     # Create the connection
     db_connection = MySQLConnection(**db_config)
 
+    cursor = db_connection.cursor()
+    cursor.execute(query_string)
+    records = cursor.fetchall()
+
+    for row in records:
+        # Data from the query:
+        internal_google_cloud_project_id = row[0]
+        google_cloud_project_name = row[1]
+        internal_google_bigquery_dataset_id = row[2]
+        google_bigquery_dataset_name = row[3]
+        google_cloud_location_name = row[4]
+        google_bigquery_dataset_default_table_expiration = row[5]
+        google_bigquery_dataset_default_partition_expiration = row[6]
+        google_bigquery_dataset_short_description = row[7]
 
 
+        # Make some vars for the subsequent API calls:
+        dataset_id = '{}.{}'.format(google_cloud_project_name, google_bigquery_dataset_name)
 
+        # See if the dataset exists:
+        if dataset_exists(dataset_id) == 1:
+            # Dataset Exists:
+            api_call_logger_message = 'Dataset already exists'
+            api_call_logger('Create', internal_google_cloud_project_id, internal_google_bigquery_dataset_id,
+                            api_call_logger_message)
 
+        else:
+            # Dataset does not exist create it:
+            print('*************************** CREATING DATASET ******************')
+            create_dataset(
+                dataset_id,
+                google_cloud_location_name,
+                google_bigquery_dataset_default_table_expiration,
+                google_bigquery_dataset_default_partition_expiration,
+                google_bigquery_dataset_short_description
+            )
 
 #####################################################
 # Run the Job:
